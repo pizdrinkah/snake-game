921fc1eeca5f8341f4afec2a781511be4a3c51da
Die Lebensmittel ist mein Lieblingsfach! <111008447+pizdrinkah@users.noreply.github.com>
2025-08-13 18:03:33 +0100

    Delete build directory

--- build/CMakeFiles/defs.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for defs.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/defs.dir/src/defs.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/defs.dir/src/defs.cpp.o: \
- /run/mount/big-drive/programming-stuff/cpp-stuff/snake-game/src/defs.cpp \
- /usr/include/stdc-predef.h
